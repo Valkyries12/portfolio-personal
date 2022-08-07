68d18d402272c32e473ef1bdb2c51d89038b8189
ncaruso <nico.caruso1992@outlook.com>
2022-08-07 20:31:50 -0300

    se agrega import react de forma explicita

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
